c5b86c0ca83a4458b89d17e6ef83567d2f4e1685
sandhubot00 <sandhubot00@gmail.com>
2025-01-27 15:09:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002116325676')
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '5000')
-MAX_BTN = int(environ.get('MAX_BTN', '10'))
+MAX_BTN = int(environ.get('MAX_BTN', '8'))
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 DELETE_TIME = int(environ.get('DELETE_TIME', 300))
 IMDB = is_enabled('IMDB', False)
